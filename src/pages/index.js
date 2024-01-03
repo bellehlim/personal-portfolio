a225354408807e4fd23a353d050b9463cf1b8939
bellehlim@gmail.com <bellehlim@gmail.com>
2024-01-03 15:39:48 -0500

    retry broken asset

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import experiences from "../content/experiences"
 import ProjectCard from "../components/project-card"
 import ProjectListItem from "../components/project-list-item"
 import headshot from "../images/headshot.jpeg"
-import linkedin from "../images/linkedin.png"
+import linkedin from "../images/linked.png"
 import email from "../images/email.png"
 
 const IndexPage = () => {
